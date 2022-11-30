5848b6e5c10906468e294f737dd96c119725fd68
quan19tomosia <tt.quan.nguyen@tomosia.com>
2022-11-30 09:59:28 +0700

    Fixed send mail to teacher feature

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,10 @@ class PagesController < ApplicationController
     @courses = ClassRoom.all
     @users = User.where.not(role: 0)
     @user_courses = ClassRoom.joins(:users).where(class_rooms_users: {user_id: current_user.id})
+    @teachers = []
+    @user_courses.each do |user_course|
+      @teachers << User.role_teacher.joins(:class_rooms).where(class_rooms_users: {class_room_id: user_course.id})
+    end
+    @teachers.flatten!
   end
 end
